d3238efe28bc21dbae38ba0de7fcb22bce3fc499
Jakub Lebiodzinski <jakleb000@pbs.edu.pl>
2024-01-15 19:38:00 +0100

    Zadanie 7

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 def add_numbers(a, b):
     return a + b
 
-
 def subtract_numbers(a, b):
     return a - b
 
-
 def multiply_numbers(a, b):
     return a * b
-
 
 def divide_numbers(a, b):
     if b == 0:
         raise ValueError("Dzielenie przez zero jest niedozwolone.")
     return a / b
-
 
 if __name__ == "__main__":
     num1 = 10
@@ -23,4 +19,3 @@ if __name__ == "__main__":
     print(f"Odejmowanie: {subtract_numbers(num1, num2)}")
     print(f"Mnożenie: {multiply_numbers(num1, num2)}")
     print(f"Dzielenie: {divide_numbers(num1, num2)}")
-
